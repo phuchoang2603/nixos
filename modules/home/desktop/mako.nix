16c381b726623121f1d9cf7921fba39192e846f0
felix <xuanphuc.a1gv@gmail.com>
2026-01-26 16:03:52 -0500

    fix theming

--- modules/home/desktop/mako.nix
+++ modules/home/desktop/mako.nix
@@ -5,16 +5,12 @@
     enable = true;
 
     settings = {
-      backgroundColor = "#${config.lib.stylix.colors.base00}DD";  # with transparency
-      textColor = "#${config.lib.stylix.colors.base05}";
-      borderColor = "#${config.lib.stylix.colors.base0A}";
       width = 420;
       height = 110;
       padding = 8;
       margin = 2;
       borderSize = 1;
       borderRadius = 12;
-      font = "CaskaydiaMono Nerd Font 11";
       anchor = "top-center";
       defaultTimeout = 5000;
       icons = true;

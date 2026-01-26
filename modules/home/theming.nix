3a3f6c28e11ea021a684138bbdea07955566e3a1
felix <xuanphuc.a1gv@gmail.com>
2026-01-26 11:46:32 -0500

    Fix Rofi theme conflict with Stylix
    - Disable Stylix's rofi theming to avoid conflicts
    - Use our custom rofi.nix theme with Stylix colors

--- modules/home/theming.nix
+++ modules/home/theming.nix
@@ -5,5 +5,17 @@
     enable = true;
     image = ../../current.png;
     polarity = "dark";
+    
+    targets = {
+      rofi = {
+        enable = false;  # We'll use our custom rofi.nix instead
+      };
+      mako = {
+        enable = false;  # We'll use our custom mako.nix instead
+      };
+      waybar = {
+        enable = false;  # We'll use our custom waybar.nix instead
+      };
+    };
   };
 }

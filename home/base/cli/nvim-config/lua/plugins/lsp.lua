c358573ecc2f9e0a511ca8a7c8c380f043865735
felix <xuanphuc.a1gv@gmail.com>
2026-02-02 14:51:56 -0500

    replace lang installed in system to devshell

---
+++ home/base/cli/nvim-config/lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Disable nil_ls
+        nil_ls = { enabled = false },
+
+        -- Enable nixd
+        nixd = {
+          settings = {
+            nixd = {
+              formatting = {
+                command = { "nixfmt" },
+              },
+              nixpkgs = {
+                expr = "import (builtins.getFlake(toString ./.)).inputs.nixpkgs { }",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}

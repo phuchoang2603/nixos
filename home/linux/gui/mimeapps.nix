9e8d8f85a55eb17506dbe754b5403fc3eb6d522f
felix <xuanphuc.a1gv@gmail.com>
2026-02-04 05:45:26 -0500

    fix small stuff

--- home/linux/gui/mimeapps.nix
+++ home/linux/gui/mimeapps.nix
@@ -4,7 +4,9 @@
     defaultApplications = {
       "application/pdf" = [ "org.pwmt.zathura.desktop" ];
       "application/x-pdf" = [ "org.pwmt.zathura.desktop" ];
-      "image/*" = [ "swappy.desktop" ];
+      "image/png" = [ "swappy.desktop" ];
+      "image/jpeg" = [ "swappy.desktop" ];
+      "image/jpg" = [ "swappy.desktop" ];
       "text/html" = [ "microsoft-edge.desktop" ];
       "x-scheme-handler/http" = [ "microsoft-edge.desktop" ];
       "x-scheme-handler/https" = [ "microsoft-edge.desktop" ];

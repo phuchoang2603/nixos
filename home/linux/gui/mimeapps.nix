b50831c09e787f29bdf8c21e343f5e7d21d48db5
felix <xuanphuc.a1gv@gmail.com>
2026-02-04 05:31:38 -0500

    include mimeapps

---
+++ home/linux/gui/mimeapps.nix
@@ -0,0 +1,13 @@
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+      "application/x-pdf" = [ "org.pwmt.zathura.desktop" ];
+      "image/*" = [ "swappy.desktop" ];
+      "text/html" = [ "microsoft-edge.desktop" ];
+      "x-scheme-handler/http" = [ "microsoft-edge.desktop" ];
+      "x-scheme-handler/https" = [ "microsoft-edge.desktop" ];
+    };
+  };
+}

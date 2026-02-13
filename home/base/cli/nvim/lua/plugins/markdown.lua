ea1369971c6c56192251c653438d6e98f3fc1b7c
felix <xuanphuc.a1gv@gmail.com>
2026-02-13 00:41:58 -0500

    fix some nvim stuff

--- home/base/cli/nvim/lua/plugins/markdown.lua
+++ home/base/cli/nvim/lua/plugins/markdown.lua
@@ -1,13 +1,29 @@
 return {
   {
     'mfussenegger/nvim-lint',
-    opts = {
-      linters = {
-        ['markdownlint-cli2'] = {
-          prepend_args = { '--config', vim.env.HOME .. '/.config/nvim/lua/config/.markdownlint-cli2.yaml', '--' },
-        },
-      },
-    },
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require('lint')
+      
+      -- Configure linters by filetype
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint-cli2' },
+      }
+      
+      -- Customize markdownlint-cli2 linter
+      lint.linters['markdownlint-cli2'].args = {
+        '--config',
+        vim.env.HOME .. '/.config/nvim/lua/config/.markdownlint-cli2.yaml',
+        '--',
+      }
+      
+      -- Auto-lint on events
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
   },
   {
     'arminveres/md-pdf.nvim',
--- home/base/cli/nvim/lua/plugins/mini.lua
+++ home/base/cli/nvim/lua/plugins/mini.lua
@@ -1,10 +1,36 @@
 return {
-  'nvim-mini/mini.nvim',
-  config = function()
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    require('mini.surround').setup()
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
 
-    -- Auto pairs
-    require('mini.pairs').setup()
-  end,
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- Examples:
+      --  - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      --  - sd'   - [S]urround [D]elete [']quotes
+      --  - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Auto pairs
+      require('mini.pairs').setup()
+
+      -- Simple and easy statusline (if you prefer over lualine)
+      -- require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can also explore other mini modules:
+      -- require('mini.comment').setup()  -- Better commenting (if you don't use Comment.nvim)
+      -- require('mini.bracketed').setup()  -- Navigate through various brackets
+      -- require('mini.bufremove').setup()  -- Better buffer deletion
+      -- require('mini.move').setup()  -- Move lines and selections
+      -- require('mini.splitjoin').setup()  -- Split and join arguments
+    end,
+  },
 }

3b1630b85d97b45c6e8376f11958b9ec4c7c3a5d
felix <xuanphuc.a1gv@gmail.com>
2026-02-13 01:12:59 -0500

    fix some stuff

--- home/base/cli/nvim/lua/plugins/mini.lua
+++ home/base/cli/nvim/lua/plugins/mini.lua
@@ -14,23 +14,25 @@ return {
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- Examples:
-      --  - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      --  - sd'   - [S]urround [D]elete [']quotes
-      --  - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'gsa', -- Add surrounding in Normal and Visual modes
+          delete = 'gsd', -- Delete surrounding
+          find = 'gsf', -- Find surrounding (to the right)
+          find_left = 'gsF', -- Find surrounding (to the left)
+          highlight = 'gsh', -- Highlight surrounding
+          replace = 'gsr', -- Replace surrounding
+          update_n_lines = 'gsn', -- Update `n_lines`
+        },
+      }
 
       -- Auto pairs
       require('mini.pairs').setup()
 
-      -- Simple and easy statusline (if you prefer over lualine)
-      -- require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
+      -- Simple and easy statusline
+      require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
 
-      -- You can also explore other mini modules:
-      -- require('mini.comment').setup()  -- Better commenting (if you don't use Comment.nvim)
-      -- require('mini.bracketed').setup()  -- Navigate through various brackets
-      -- require('mini.bufremove').setup()  -- Better buffer deletion
-      -- require('mini.move').setup()  -- Move lines and selections
-      -- require('mini.splitjoin').setup()  -- Split and join arguments
+      require('mini.base16').setup { use_cterm = true }
     end,
   },
 }

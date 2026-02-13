300e643f6fc1537e6f3f0ff7b49658caef432f46
felix <xuanphuc.a1gv@gmail.com>
2026-02-13 04:18:35 -0500

    migrate to lsp config

--- home/base/cli/nvim/lua/config/servers.lua
+++ home/base/cli/nvim/lua/config/servers.lua
@@ -10,26 +10,29 @@ function M.get_servers()
   return {
     -- Lua
     lua_ls = {
-      root_dir = get_root_dir { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
-      on_init = function(client)
-        if client.workspace_folders then
-          local path = client.workspace_folders[1].name
-          if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
-        end
-
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-          runtime = {
-            version = 'LuaJIT',
-            path = { 'lua/?.lua', 'lua/?/init.lua' },
-          },
+      settings = {
+        Lua = {
           workspace = {
             checkThirdParty = false,
-            library = vim.api.nvim_get_runtime_file('', true),
           },
-        })
-      end,
-      settings = {
-        Lua = {},
+          codeLens = {
+            enable = true,
+          },
+          completion = {
+            callSnippet = 'Replace',
+          },
+          doc = {
+            privateName = { '^_' },
+          },
+          hint = {
+            enable = true,
+            setType = false,
+            paramType = true,
+            paramName = 'Disable',
+            semicolon = 'Disable',
+            arrayIndex = 'Disable',
+          },
+        },
       },
     },
 

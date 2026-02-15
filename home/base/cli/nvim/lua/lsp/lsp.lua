831e793a9f9a9587e708d57724573e0b3f65f18b
felix <xuanphuc.a1gv@gmail.com>
2026-02-15 11:41:05 -0500

    enhance UI stuff

--- home/base/cli/nvim/lua/lsp/lsp.lua
+++ home/base/cli/nvim/lua/lsp/lsp.lua
@@ -35,10 +35,10 @@ local spec = {
           severity_sort = true,
           signs = {
             text = {
-              [vim.diagnostic.severity.ERROR] = 'E',
-              [vim.diagnostic.severity.WARN] = 'W',
-              [vim.diagnostic.severity.HINT] = 'H',
-              [vim.diagnostic.severity.INFO] = 'I',
+              [vim.diagnostic.severity.ERROR] = '󰅚 ',
+              [vim.diagnostic.severity.WARN] = '󰀪 ',
+              [vim.diagnostic.severity.HINT] = '󰌶 ',
+              [vim.diagnostic.severity.INFO] = '󰋽 ',
             },
           },
         },
--- home/base/cli/nvim/lua/plugins/mini.lua
+++ home/base/cli/nvim/lua/plugins/mini.lua
@@ -29,8 +29,36 @@ return {
       -- Auto pairs
       require('mini.pairs').setup()
 
+      -- Icons
+      require('mini.icons').setup()
+
       -- Simple and easy statusline
-      require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
+      local statusline = require 'mini.statusline'
+      statusline.setup {
+        use_icons = vim.g.have_nerd_font,
+        content = {
+          active = function()
+            local mode, mode_hl = statusline.section_mode { trunc_width = 120 }
+            local diff = statusline.section_diff { trunc_width = 75 }
+            local diagnostics = statusline.section_diagnostics { trunc_width = 75 }
+            local lsp = statusline.section_lsp { trunc_width = 75 }
+            local filename = statusline.section_filename { trunc_width = 140 }
+            local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+            local location = statusline.section_location { trunc_width = 75 }
+            local search = statusline.section_searchcount { trunc_width = 75 }
+
+            return statusline.combine_groups {
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo', strings = { diff, diagnostics, lsp } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            }
+          end,
+        },
+      }
     end,
   },
 }

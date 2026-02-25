aa3ce76d1141a74a395deaf9478fb09db0ac2381
felix <xuanphuc.a1gv@gmail.com>
2026-02-25 11:25:06 -0500

    add swipe gesture for aerospace
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- home/darwin/aerospace.nix
+++ home/darwin/aerospace.nix
@@ -22,6 +22,10 @@
         preset = "qwerty";
       };
 
+      "workspace-to-monitor-force-assignment" = {
+        "10" = "DELL P2317H";
+      };
+
       gaps = {
         inner.horizontal = 2;
         inner.vertical = 4;
@@ -43,6 +47,9 @@
         alt-5 = "workspace 5";
         alt-6 = "workspace 6";
         alt-7 = "workspace 7";
+        alt-8 = "workspace 8";
+        alt-9 = "workspace 9";
+        alt-0 = "workspace 10";
 
         # Focus
         alt-h = "focus left";
@@ -60,12 +67,46 @@
         alt-shift-p = "workspace --wrap-around prev";
 
         # Move window to workspace
-        alt-shift-1 = "move-node-to-workspace 1";
-        alt-shift-2 = "move-node-to-workspace 2";
-        alt-shift-3 = "move-node-to-workspace 3";
-        alt-shift-4 = "move-node-to-workspace 4";
-        alt-shift-5 = "move-node-to-workspace 5";
-        alt-shift-6 = "move-node-to-workspace 6";
+        alt-shift-1 = [
+          "move-node-to-workspace 1"
+          "workspace 1"
+        ];
+        alt-shift-2 = [
+          "move-node-to-workspace 2"
+          "workspace 2"
+        ];
+        alt-shift-3 = [
+          "move-node-to-workspace 3"
+          "workspace 3"
+        ];
+        alt-shift-4 = [
+          "move-node-to-workspace 4"
+          "workspace 4"
+        ];
+        alt-shift-5 = [
+          "move-node-to-workspace 5"
+          "workspace 5"
+        ];
+        alt-shift-6 = [
+          "move-node-to-workspace 6"
+          "workspace 6"
+        ];
+        alt-shift-7 = [
+          "move-node-to-workspace 7"
+          "workspace 7"
+        ];
+        alt-shift-8 = [
+          "move-node-to-workspace 8"
+          "workspace 8"
+        ];
+        alt-shift-9 = [
+          "move-node-to-workspace 9"
+          "workspace 9"
+        ];
+        alt-shift-0 = [
+          "move-node-to-workspace 10"
+          "workspace 10"
+        ];
 
         # Move window to monitor
         alt-shift-left = "move-node-to-monitor left";

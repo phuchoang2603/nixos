79d69c48dfacdf40191f44c3cd831de06cdb215d
felix <xuanphuc.a1gv@gmail.com>
2026-02-24 23:37:15 -0500

    restructure package
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

---
+++ home/linux/packages.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, ... }:
+
+{
+  home.packages = with pkgs; [
+    microsoft-edge
+    vscode
+    jetbrains.datagrip
+    obsidian
+    inotify-tools
+    vlc
+    todoist
+    libreoffice-fresh
+    playerctl
+    gimp
+    zathura
+
+    # Wayland Essentials
+    wayland
+    wayland-utils
+    wayland-protocols
+    wl-clipboard
+    wlr-randr
+    libnotify
+
+    # Qt Theming
+    qt5.qtwayland
+    qt6.qtwayland
+    libsForQt5.qt5ct
+    qt6Packages.qt6ct
+
+    # Linux Utilities
+    mission-center
+    gnome-calculator
+    gnome-clocks
+    baobab
+    sushi
+    localsend
+    blueman
+    pciutils
+    usbutils
+    nautilus
+  ];
+}

f3ca183a9edadb6c63d82d30d3d5af27b82c4054
felix <xuanphuc.a1gv@gmail.com>
2026-02-27 14:03:59 -0500

    use the built-in snippets
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- home/base/cli/nvim/lua/plugins/blink.lua
+++ home/base/cli/nvim/lua/plugins/blink.lua
@@ -5,32 +5,35 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 	group = group,
 	once = true,
 	callback = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
 		require("blink.cmp").setup({
 			keymap = {
-				preset = "enter",
-
-				["<Tab>"] = { "fallback" },
-				["<S-Tab>"] = { "fallback" },
+				preset = "none",
+				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
 
 				["<C-l>"] = { "snippet_forward", "fallback" },
 				["<C-h>"] = { "snippet_backward", "fallback" },
 
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-e>"] = { "hide" },
+				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+				["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
 			},
 			appearance = {
 				nerd_font_variant = "mono",
-				use_nvim_cmp_as_default = true,
 			},
 			completion = {
+				list = { selection = { preselect = false, auto_insert = false } },
 				documentation = { auto_show = false, auto_show_delay_ms = 500 },
 				ghost_text = { enabled = false },
 			},
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
 			},
-			snippets = { preset = "luasnip" },
 			fuzzy = { implementation = "rust" },
 			signature = { enabled = true },
 		})

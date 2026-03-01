26f357ba05c111d8bc0f9ceb5bd7726e903ee69b
felix <xuanphuc.a1gv@gmail.com>
2026-03-01 02:07:23 -0500

    config copilot
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- home/base/cli/nvim/lua/config/lsp.lua
+++ home/base/cli/nvim/lua/config/lsp.lua
@@ -2,6 +2,48 @@ local function augroup(name)
 	return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
 end
 
+-- Helper for Partial Word Acceptance
+function AcceptCompletion(item)
+	local insert_text = item.insert_text
+	if type(insert_text) == "string" then
+		local range = item.range
+		if range then
+			local lines = vim.split(insert_text, "\n")
+			local current_lines = vim.api.nvim_buf_get_text(
+				range.start.buf,
+				range.start.row,
+				range.start.col,
+				range.end_.row,
+				range.end_.col,
+				{}
+			)
+
+			local row = 1
+			while row <= #lines and row <= #current_lines and lines[row] == current_lines[row] do
+				row = row + 1
+			end
+
+			local col = 1
+			while
+				row <= #lines
+				and col <= #lines[row]
+				and row <= #current_lines
+				and col <= #current_lines[row]
+				and lines[row][col] == current_lines[row][col]
+			do
+				col = col + 1
+			end
+
+			local word = string.match(lines[row]:sub(col), "%s*[^%s]%w*")
+			item.insert_text = table.concat(vim.list_slice(lines, 1, row - 1), "\n")
+				.. (row <= #current_lines and "" or "\n")
+				.. (row <= #lines and col <= #lines[row] and lines[row]:sub(1, col - 1) or "")
+				.. word
+		end
+	end
+	return item
+end
+
 -- LSP
 local servers = {
 	"basedpyright",
@@ -30,6 +72,9 @@ local default_keymaps = {
 	{ keys = "<leader>ca", func = vim.lsp.buf.code_action, desc = "Code Actions" },
 	{ keys = "<leader>cr", func = vim.lsp.buf.rename, desc = "Code Rename" },
 	{ keys = "<leader>cf", func = vim.lsp.buf.format, desc = "Code Format" },
+	{ keys = "<leader>k", func = vim.lsp.buf.hover, desc = "Hover Documentation", has = "hoverProvider" },
+	{ keys = "K", func = vim.lsp.buf.hover, desc = "Hover (alt)", has = "hoverProvider" },
+	{ keys = "gd", func = vim.lsp.buf.definition, desc = "Goto Definition", has = "definitionProvider" },
 	{
 		keys = "<leader>uc",
 		func = function()
@@ -43,18 +88,50 @@ local default_keymaps = {
 		mode = "i",
 		keys = "<Tab>",
 		func = function()
-			if vim.lsp.inline_completion.get() then
-				return vim.lsp.inline_completion.accept()
+			-- If it's already enabled, try to accept a suggestion
+			if vim.lsp.inline_completion.is_enabled() then
+				if vim.lsp.inline_completion.get() then
+					return ""
+				end
 			else
-				return "<Tab>"
+				-- If disabled, enable it and trigger a request
+				vim.lsp.inline_completion.enable(true)
+				return ""
 			end
+			return "<Tab>"
 		end,
 		expr = true,
 		desc = "Accept Copilot Suggestions or Tab",
 	},
-	{ keys = "<leader>k", func = vim.lsp.buf.hover, desc = "Hover Documentation", has = "hoverProvider" },
-	{ keys = "K", func = vim.lsp.buf.hover, desc = "Hover (alt)", has = "hoverProvider" },
-	{ keys = "gd", func = vim.lsp.buf.definition, desc = "Goto Definition", has = "definitionProvider" },
+	{
+		mode = "i",
+		keys = "<C-j>",
+		func = function()
+			vim.lsp.inline_completion.select({ count = 1 })
+		end,
+		desc = "Next Copilot Suggestion",
+	},
+	{
+		mode = "i",
+		keys = "<C-k>",
+		func = function()
+			vim.lsp.inline_completion.select({ count = -1 })
+		end,
+		desc = "Previous Copilot Suggestion",
+	},
+	{
+		mode = "i",
+		keys = "<C-b>",
+		func = function()
+			local result = vim.lsp.inline_completion.get({ on_accept = AcceptCompletion })
+			if not result then
+				return "<C-b>"
+			end
+		end,
+		expr = true,
+		replace_keycodes = true,
+		desc = "Accept Next Word of Copilot Suggestions",
+	},
 }
 
 -- Events
@@ -64,11 +141,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		local buf = args.buf
 		if client then
-			-- Native Inline Completion
-			if client:supports_method("textDocument/inlineCompletion") then
-				vim.lsp.inline_completion.enable(false, { bufnr = buf })
-			end
-
 			-- Inlay hints
 			if client:supports_method("textDocument/inlayHint") then
 				vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })

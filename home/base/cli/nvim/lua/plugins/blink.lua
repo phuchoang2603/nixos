2a64fdffd341a2f5d4d4b06cdfb03a1bd32c4e32
felix <xuanphuc.a1gv@gmail.com>
2026-03-10 17:52:48 -0400

    fix blink
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- home/base/cli/nvim/lua/plugins/blink.lua
+++ home/base/cli/nvim/lua/plugins/blink.lua
@@ -27,8 +27,7 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 				nerd_font_variant = "mono",
 			},
 			completion = {
-				list = { selection = { preselect = false, auto_insert = false } },
-				documentation = { auto_show = false, auto_show_delay_ms = 500 },
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
 				ghost_text = { enabled = false },
 			},
 			sources = {

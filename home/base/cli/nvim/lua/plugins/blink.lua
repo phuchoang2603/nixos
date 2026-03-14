85bfe7c090fe83c0f59809a8649b0bfead49d3d1
felix <xuanphuc.a1gv@gmail.com>
2026-03-14 12:11:09 -0400

    disable lazy load blink
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- home/base/cli/nvim/lua/plugins/blink.lua
+++ home/base/cli/nvim/lua/plugins/blink.lua
@@ -1,40 +1,31 @@
-local group = vim.api.nvim_create_augroup("BlinkCmpLazyLoad", { clear = true })
+require("blink.cmp").setup({
+	keymap = {
+		preset = "none",
+		["<C-e>"] = { "show", "hide" },
+		["<C-space>"] = { "show_documentation", "hide_documentation" },
+		["<CR>"] = { "accept", "fallback" },
 
-vim.api.nvim_create_autocmd("InsertEnter", {
-	pattern = "*",
-	group = group,
-	once = true,
-	callback = function()
-		require("blink.cmp").setup({
-			keymap = {
-				preset = "none",
-				["<C-e>"] = { "show", "hide" },
-				["<C-space>"] = { "show_documentation", "hide_documentation" },
-				["<CR>"] = { "accept", "fallback" },
+		["<C-l>"] = { "snippet_forward", "fallback" },
+		["<C-h>"] = { "snippet_backward", "fallback" },
 
-				["<C-l>"] = { "snippet_forward", "fallback" },
-				["<C-h>"] = { "snippet_backward", "fallback" },
+		["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+		["<C-n>"] = { "select_next", "fallback_to_mappings" },
 
-				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-				["<C-n>"] = { "select_next", "fallback_to_mappings" },
+		["<C-b>"] = { "scroll_documentation_up", "fallback" },
+		["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
-				["<C-b>"] = { "scroll_documentation_up", "fallback" },
-				["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-			},
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-			completion = {
-				documentation = { auto_show = true, auto_show_delay_ms = 500 },
-				ghost_text = { enabled = false },
-			},
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-			fuzzy = { implementation = "rust" },
-			signature = { enabled = true },
-		})
-	end,
+		["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+	},
+	appearance = {
+		nerd_font_variant = "mono",
+	},
+	completion = {
+		documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		ghost_text = { enabled = false },
+	},
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
+	fuzzy = { implementation = "rust" },
+	signature = { enabled = true },
 })

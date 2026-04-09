13d77eeab0faac92f144b40a3ed0a950eac29b07
felix <xuanphuc.a1gv@gmail.com>
2026-04-09 17:07:20 -0400

    add nvidia to nixos-server
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- hosts/nixos-server/default.nix
+++ hosts/nixos-server/default.nix
@@ -5,8 +5,8 @@
     ./hardware-configuration.nix
     ../../modules/nixos/base.nix
     ../../modules/nixos/server-services.nix
+    ../../modules/nixos/nvidia.nix
   ];
 
   networking.hostName = "nixos-server";
-  programs.nix-ld.enable = true;
 }

c177d0d6b0cad76c42273760362ab850f0ae55f1
felix <xuanphuc.a1gv@gmail.com>
2026-06-15 09:51:54 +0700

    remove cursor cli and acp for opencode
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- home/base/cli/ai.nix
+++ home/base/cli/ai.nix
@@ -3,7 +3,6 @@
 {
   home.packages = with pkgs; [
     pi-coding-agent
-    cursor-cli
     openspec
   ];
 
@@ -15,16 +14,16 @@
       skills = {
         find-skills = ./skills/find-skills;
       };
-      web = {
-        enable = true;
-        extraArgs = [
-          "--hostname"
-          "0.0.0.0"
-          "--port"
-          "4096"
-          "--mdns"
-        ];
-      };
+      # web = {
+      #   enable = true;
+      #   extraArgs = [
+      #     "--hostname"
+      #     "0.0.0.0"
+      #     "--port"
+      #     "4096"
+      #     "--mdns"
+      #   ];
+      # };
       tui = {
         keybinds = {
           messages_half_page_up = "{";
@@ -35,44 +34,6 @@
         permission = {
           edit = "ask";
           bash = "ask";
-        };
-        plugin = [ "@rama_nigg/open-cursor@latest" ];
-        provider = {
-          cursor-acp = {
-            name = "Cursor ACP";
-            npm = "@ai-sdk/openai-compatible";
-
-            options = {
-              baseURL = "http://127.0.0.1:32124/v1";
-            };
-
-            models = {
-              "cursor-acp/auto" = {
-                name = "Auto";
-              };
-
-              "cursor-acp/claude-opus-4-7" = {
-                name = "Claude 4.7 Opus";
-              };
-
-              "cursor-acp/gpt-5.5" = {
-                name = "GPT-5.5";
-              };
-              "cursor-acp/gpt-5.4" = {
-                name = "GPT-5.4";
-              };
-              "cursor-acp/gpt-5.4-mini" = {
-                name = "GPT-5.4 Mini";
-              };
-
-              "cursor-acp/composer-2.5" = {
-                name = "Composer 2.5";
-              };
-              "cursor-acp/composer-2.5-fast" = {
-                name = "Composer 2.5 Fast";
-              };
-            };
-          };
         };
       };
     };

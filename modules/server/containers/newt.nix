109fce5b6a6936d234cf4dd9194ac86c32efde7a
felix <xuanphuc.a1gv@gmail.com>
2026-09-16 13:09:03 -0400

    add t3code to pangolin
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- modules/server/containers/newt.nix
+++ modules/server/containers/newt.nix
@@ -3,6 +3,7 @@
 {
   virtualisation.oci-containers.containers.newt = lab.mkContainer {
     image = "fosrl/newt";
+    extraOptions = [ "--add-host=host.docker.internal:host-gateway" ];
     environmentFiles = [ "${lab.secrets}/newt.env" ];
   };
 }
